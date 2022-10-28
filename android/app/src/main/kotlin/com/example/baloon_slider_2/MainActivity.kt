4a9e84f23c48e1d4b97d643024446035d031c8ea
himym1989 <himym1989@gmail.com>
2022-10-28 15:16:03 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/baloon_slider_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baloon_slider_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:baloon_slider_2/baloon_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MySlider(),
+    );
+  }
+}
